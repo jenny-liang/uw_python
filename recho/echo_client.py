2db6774941151daeea80c57920661bbf5671db6e
jenny-liang <jennyliang@ubuntu.(none)>
2012-01-11 22:51:52 -0800

    Add recho client to repeatly prompt a message to send until the user hit enter.

---
+++ recho/echo_client.py
@@ -0,0 +1,35 @@
+"""
+echo client, usage:
+
+ python echo_client.py <host> <port>
+
+Both host and port are optional, defaults: localhost 50000
+host must be present if you want to provide port
+"""
+import socket 
+import sys
+
+host = 'localhost' 
+port = 50000 
+size = 1024 
+
+nargs = len(sys.argv)
+if nargs > 1:
+    host = sys.argv[1]
+if nargs > 2:
+    port = int(sys.argv[2])
+
+
+while True:
+    s = socket.socket(socket.AF_INET, 
+                  socket.SOCK_STREAM) 
+    s.connect((host,port))
+    data = raw_input("Enter a message to send: ")
+    if data == '':
+        break
+    s.send(data)
+    data = s.recv(size)
+    print 'from (%s,%s) %s' % (host, port, data)   
+s.close() 
+
+
---
+++ recho/echo_server.py
@@ -0,0 +1,40 @@
+"""
+echo server, usage:
+
+ python echo_server.py <port>
+
+Port is optional, default: 50000
+"""
+
+import socket 
+import sys
+
+host = '' 
+port = 50000 
+
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+
+backlog = 5 
+size = 1024
+
+# server's listener socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # SOCK_STREAM -- TCPIP POTOCAL 
+
+# Release listener socket immediately when program exits, 
+# avoid socket.error: [Errno 48] Address already in use
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind((host,port)) 
+
+print 'echo_server listening on port', port
+s.listen(backlog) 
+
+while True: 
+    client, address = s.accept() #socket is bidirectional: return client socket talking to that client with its address
+    data = client.recv(size) 
+    if data:
+	client.send('jenny-liang: %s' % data) # Tell the client which server is connected.
+    print 'from %s: %s' % (address, data)
+    client.close()  # close the connection and wait for another client to talk to another client.
+
